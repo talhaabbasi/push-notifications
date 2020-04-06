990dcdfd788628a6c13cd29a6ccb74d81d743947
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-04-07 02:19:17 +0500

    Push Notification setup
    The web-push package was installed, Vapid keys were generated and
    integrated with in the application and the subscriber action was setup

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const webpush = require("web-push");
+const bodyParser = require("body-parser");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const publicVapidKey = process.env.publicVapidKey;
+const privateVapidKey = process.env.privateVapidKey;
+
+webpush.setVapidDetails(
+  "mailto:test@test.com",
+  publicVapidKey,
+  privateVapidKey
+);
+
+app.post("/subscribe", (req, res, next) => {
+  // Get push subscription object
+  const subscription = req.body;
+
+  res.status(201).json({});
+
+  const payload = JSON.stringify({ title: "Push test" });
+
+  webpush.sendNotification(subscription, payload).catch((err) => {
+    console.error(err);
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
